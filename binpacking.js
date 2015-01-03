4eb27584655f9a45962f3ca8183c1cee6c06cc4d
Loïc GAILLARD <gaillard.log@gmail.com>
2015-01-03 19:59:01 +0100

    added file reader

--- binpacking.js
+++ binpacking.js
@@ -1,4 +1,31 @@
+var binpacking = {
+    handleFileReader : function() {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            $("#file_input").on("change", this.readFile);
+        } else {
+            //alert('The File APIs are not fully supported in this browser.');
+            $("#file_input").hide().after("<p>File Reader isn't supported by your navigator</p>");
+        }
+    },
+
+    readFile : function(event) {
+        var file = event.target;
+
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(file) {
+            $("#input").val(fileReader.result);
+        };
+
+        fileReader.readAsText(file.files[0]);
+    },
+
+    initialize : function() {
+        this.handleFileReader();
+    }
+};
+
+
 $( document ).ready(function() {
-
-
+    binpacking.initialize();
 });
