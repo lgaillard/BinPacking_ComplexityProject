dcf79124bc95209d873711a63ed81de6b3dbf9f3
Loïc GAILLARD <gaillard.log@gmail.com>
2015-01-04 05:27:15 +0100

    reading input

--- binpacking.js
+++ binpacking.js
@@ -1,4 +1,19 @@
-var binpacking = {
+var Bin = function(w, h) {
+    this.width = w;
+    this.height = h;
+};
+
+var Piece = function(w, h) {
+    this.width = w;
+    this.height = h;
+};
+
+
+var BinPackingApp = {
+    binW : 0,
+    binH : 0,
+    pieces : Array(),
+
     handleFileReader : function() {
         if (window.File && window.FileReader && window.FileList && window.Blob) {
             $("#file_input").on("change", this.readFile);
@@ -20,17 +35,64 @@ var binpacking = {
         fileReader.readAsText(file.files[0]);
     },
 
+    readInput : function() {
+        var input = $("#input").val();
+        var lines = input.split("\n");
+        if(lines.length < 2) {
+            this.wrongInput();
+            return;
+        }
+
+        // Bin Size
+        var binSize = lines[0].split("x");
+        if(binSize.length != 2) {
+            this.wrongInput();
+            return;
+        }
+
+        this.binW = binSize[0];
+        this.binH = binSize[1];
+
+
+        // Pieces
+        var pieces = lines[1].split(",");
+        for(var i = 0; i < pieces.length; ++i) {
+            var pieceSize = pieces[i].split("x");
+            if(pieceSize.length != 2) {
+                this.wrongInput();
+                return;
+            }
+
+            this.pieces.push(new Piece(pieceSize[0], pieceSize[1]));
+        }
+    },
+
+    wrongInput : function() {
+        // TODO
+        alert("Wrong Input");
+    },
+
+    clearData : function() {
+        this.binW = 0;
+        this.binH = 0;
+        this.pieces = Array();
+    },
+
     initialize : function() {
+        var self = this;
+
         this.handleFileReader();
-        $("#run_button").click(this.run);
+        $("#run_button").click(function(){
+            self.run();
+        });
     },
 
     run : function() {
-
+        this.readInput();
     }
 };
 
 
 $( document ).ready(function() {
-    binpacking.initialize();
+    BinPackingApp.initialize();
 });
