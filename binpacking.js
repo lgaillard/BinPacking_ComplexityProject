f8a0d04f2cd47c50c2136dea6c3ef56563570b56
Loïc GAILLARD <gaillard.log@gmail.com>
2015-01-04 16:38:24 +0100

    revamp canvas into html div Creating bins, adding pieces

--- binpacking.js
+++ binpacking.js
@@ -1,6 +1,42 @@
+var Settings = {
+    cellSize: 25,
+    margin: 10
+};
+
 var Bin = function(w, h) {
     this.width = w;
     this.height = h;
+    this.availableH = h;
+    this.layers = [];
+
+    this.gBin = $("<div>").addClass("bin").css({
+        position: "relative",
+        width: Settings.cellSize * w,
+        height: Settings.cellSize * h,
+        border: "1px solid black",
+        margin: Settings.cellSize
+    }).appendTo($("#output"));
+    this.insertPiece(new Piece(2,1), 0, 0);
+};
+
+Bin.prototype.insertPiece = function(piece, x, y) {
+    if(piece.width > this.width || piece.height > this.height) {
+        //TODO:
+        alert("Error: A piece is bigger than the bins");
+        return;
+    }
+
+    var color = "#" + String("000000" + (Math.floor(Math.random() * parseInt("FFFFFF", 16))).toString(16)).slice(-6);
+    var gPiece = $("<div>").addClass("piece").css({
+        position: "absolute",
+        left: Settings.cellSize * x,
+        bottom: Settings.cellSize * y,
+        width: Settings.cellSize * piece.width,
+        height: Settings.cellSize * piece.height,
+        background: color,
+        "box-shadow": "inset 0 0 0 1px rgba(0,0,0,0.2)"
+
+    }).appendTo(this.gBin);
 };
 
 var Piece = function(w, h) {
@@ -12,7 +48,9 @@ var Piece = function(w, h) {
 var BinPackingApp = {
     binW : 0,
     binH : 0,
-    pieces : Array(),
+    bins : [],
+    pieces : [],
+    output : null,
 
     handleFileReader : function() {
         if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -28,7 +66,7 @@ var BinPackingApp = {
 
         var fileReader = new FileReader();
 
-        fileReader.onload = function(file) {
+        fileReader.onload = function() {
             $("#input").val(fileReader.result);
         };
 
@@ -75,7 +113,20 @@ var BinPackingApp = {
     clearData : function() {
         this.binW = 0;
         this.binH = 0;
-        this.pieces = Array();
+        this.bins = [];
+        this.pieces = [];
+    },
+
+    sortPieces : function() {
+        this.pieces.sort(function(p1, p2) {
+            return p2.height - p1.height;
+        });
+    },
+
+    insertPieces : function() {
+        if(this.bins.length < 1) {
+            this.bins.push(new Bin(this.binW, this.binH));
+        }
     },
 
     initialize : function() {
@@ -85,10 +136,15 @@ var BinPackingApp = {
         $("#run_button").click(function(){
             self.run();
         });
+
+        this.output = $("#output");
     },
 
     run : function() {
+        this.clearData();
         this.readInput();
+        this.sortPieces();
+        this.insertPieces();
     }
 };
 
